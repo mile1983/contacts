17da8287284dca80ec0eff8e4ff510521118045b
mile1983 <milexic@gmail.com>
2018-01-08 20:54:02 +0100

    drugi commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsListComponent
   ],
   imports: [
     BrowserModule
