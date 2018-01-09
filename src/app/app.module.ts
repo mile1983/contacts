b0c123ee4288b73dd59df53032d641243cd70460
mile1983 <milexic@gmail.com>
2018-01-09 18:16:40 +0100

    8 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsListComponent
+    ContactsListComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
